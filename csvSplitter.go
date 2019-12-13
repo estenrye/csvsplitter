7ca51f260a3f71a0e60697f5652434604a32de9f
Esten Rye <esten.rye@ryezone.com>
2019-12-12 21:34:34 -0600

    Add logic to parse and extract data.

--- csvSplitter.go
+++ csvSplitter.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"github.com/estenrye/csvsplitter/csvsplitter"
+	"flag"
+	"fmt"
+	"os"
+)
 
+func main() {
+	splitColumn := flag.Int("splitColumn", 1, "Index of the columnt to split the file on. Default of 1 is the leftmost column.")
+	hasHeaderRow := flag.Bool("hasHeaderRow", false, "Enables splitting data with a header row.")
+	flag.Parse()
+
+	if len(flag.Args()) < 1	{
+		fmt.Print("No filename provided.")
+		return
+	}
+
+	for _,s := range(flag.Args()) {
+		f, err := os.Open(s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+
+		r := csv.NewReader(f)
+
+		fmt.Println("Reading file: " + s)
+		rows, err := r.ReadAll()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		files := csvsplitter.SplitRows(rows, *hasHeaderRow, *splitColumn - 1)
+
+		for _, data := range(files) {
+			filename := data.Key + ".csv"
+			out, err := os.Create(filename)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer out.Close()
+
+			w := csv.NewWriter(out)
+
+			if data.HeaderRow != nil {
+				w.Write(data.HeaderRow)
+			}
+			w.WriteAll(data.Rows)
+		}
+	}
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/estenrye/csvsplitter
+
+go 1.12
+
+require github.com/estenrye/csvsplitter/csvsplitter
